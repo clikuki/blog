30e23a6dd2c5531b46f735ce0b2ed667f8df819f
Lanz Vasquez <lanzvasquez24@gmail.com>
2025-11-28 22:02:43 +0800

    mdx setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 export default defineConfig({
-	site: "https://clikuki.github.io/blog",
-	base: "/blog",
+  site: "https://clikuki.github.io/blog",
+  base: "/blog",
+  integrations: [mdx({})],
 });
