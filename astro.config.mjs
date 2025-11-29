e22895027f2c3c54dca87ad7784d671c04b06113
Lanz Vasquez <lanzvasquez24@gmail.com>
2025-11-29 23:09:43 +0800

    removed mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import mdx from "@astrojs/mdx";
-
 export default defineConfig({
   site: "https://clikuki.github.io/blog",
   base: "/blog",
-  integrations: [mdx({
-		syntaxHighlight: 'shiki',
-		shikiConfig: {
-			theme: 'monokai',
-		}
-	})],
 });
