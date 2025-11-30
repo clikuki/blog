55343b50801b1156dd83746d5dccf093ddfbee69
Lanz Vasquez <lanzvasquez24@gmail.com>
2025-11-30 20:05:09 +0800

    styling + site navigation

--- src/misc.d.ts
+++ src/misc.d.ts
@@ -2,4 +2,12 @@ import type { AstroInstance } from "astro";
 
 export interface PostInstance extends AstroInstance {
   title: string;
+	date: Date;
+	excerpt: string;
+}
+
+export interface Post {
+	slug: string,
+	path: string,
+	file: PostInstance,
 }
--- src/scripts/getContent.ts
+++ src/scripts/getContent.ts
@@ -1,11 +1,5 @@
-import type { PostInstance } from "../misc";
+import type { Post, PostInstance } from "../misc";
 import Path from "path/win32";
-
-export interface Post {
-	slug: string,
-	path: string,
-	file: PostInstance,
-}
 
 const files = import.meta.glob<PostInstance>("../content/*.astro", { eager: true });
 export const posts = Object.entries(files).map<Post>(([path, file]) => ({
