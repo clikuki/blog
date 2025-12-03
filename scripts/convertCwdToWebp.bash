ed67e3a19062b5eb3600d9452d1545c37470d29b
Lanz Vasquez <lanzvasquez24@gmail.com>
2025-12-04 00:28:43 +0800

    third post added

--- scripts/convertCwdToWebp.bash
+++ scripts/convertCwdToWebp.bash
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-for img in *; do cwebp $img -o "${img%.*}".webp; done
+for img in *.jpg; do cwebp $img -o "${img%.*}".webp; done
+for img in *.png; do cwebp $img -o "${img%.*}".webp; done
